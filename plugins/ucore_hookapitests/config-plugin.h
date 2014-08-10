d6adf609cffbabdbcacf3bb3536ce1a9ad89dba1
chyyuu <yuchen@tsinghua.edu.cn>
2014-08-10 21:31:52 +0800

    fork ucore_hootapitests from hookapitests

---
+++ plugins/ucore_hookapitests/config-plugin.h
@@ -0,0 +1,6 @@
+#define DEFINE_INSN_BEGIN
+#define DEFINE_INSN_END
+#define DEFINE_BLOCK_BEGIN
+#define DEFINE_BLOCK_END
+#define DECAF_HOME "/ubuntu/chyyuu/develop/dbi/decaf/git-repo/decaf-platform"
+#define PLUGIN_PATH "/ubuntu/chyyuu/develop/dbi/decaf/git-repo/decaf-platform/plugins/hookapitests"
