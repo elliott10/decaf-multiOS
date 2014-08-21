66a5f515a33fc4449c55ee9941474001083e0230
chyyuu <yuchen@tsinghua.edu.cn>
2014-08-21 23:37:17 +0800

    update for compiling on ubuntu 14.04

--- conf-build.sh
+++ conf-build.sh
@@ -1,1 +1,2 @@
-./configure  --target-list=i386-softmmu --enable-tcg-taint --enable-tcg-ir-log --enable-vmi 2>&1|tee myconfig1.log
+make distclean
+./configure  --disable-docs --target-list=i386-softmmu --enable-tcg-taint --enable-tcg-ir-log --enable-vmi 2>&1|tee myconfig1.log
